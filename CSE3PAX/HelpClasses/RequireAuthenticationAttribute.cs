2b1bcff70a880b320dc5e4fa51fe12fd7a32ed55
michaellittle92 <michaellittle92@gmail.com>
2024-02-02 19:32:30 +1100

    Merged Work in progress
    Merged Work in progress

---
+++ CSE3PAX/HelpClasses/RequireAuthenticationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CSE3PAX.HelpClasses
+{
+    public class RequireAuthenticationAttribute : Attribute, IPageFilter
+    {
+        public string RequiredRole { get; set; } = "";
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+        {
+        }
+
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            bool isAdmin = context.HttpContext.Session.GetBoolean("isAdmin");
+            if (isAdmin == false) { 
+            
+            }
+        }
+
+        public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+        {
+
+        }
+    }
+}
