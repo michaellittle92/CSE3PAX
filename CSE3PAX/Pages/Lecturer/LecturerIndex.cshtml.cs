b52186d121cff576556b495f5426ae6dab433049
michaellittle92 <michaellittle92@gmail.com>
2024-02-02 18:36:28 +1100

    Notes in LecturerIndex
    Notes in LecturerIndex

--- CSE3PAX/Pages/Lecturer/LecturerIndex.cshtml.cs
+++ CSE3PAX/Pages/Lecturer/LecturerIndex.cshtml.cs
@@ -10,5 +10,22 @@ namespace CSE3PAX.Pages
 {
     public class LecturerIndexModel : PageModel
     {
+        //get current month and year 
+        //Array SubjectInstance {instance name, subject name, start date, end date,}
+        //Array SubjectInstances[]
+
+        //SQL Query to pull all subject instances over the next 12 months where lecturer id = lecturer id
+        // put each row into the subject instances array
+
+        //For each subjectinstance in subjectinstances 
+            //reorder by start date, if this cannot be done effectively by sql query
+
+        //get subjectinstance[0] start month
+        //create 12 column table based on start month adding +1 to month for each column. 
+        
+        //for each row subjectinstance in subject instances 
+            // 
+
+        
     }
 }
