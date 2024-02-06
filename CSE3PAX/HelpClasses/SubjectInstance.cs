8ba3ee2da1e9047aa917f81eab150cb5e28bca33
michaellittle92 <michaellittle92@gmail.com>
2024-02-06 21:57:31 +1100

    Started Work on Lecturer Index.
    still trying to work out how it should all go together.

---
+++ CSE3PAX/HelpClasses/SubjectInstance.cs
@@ -0,0 +1,12 @@
+﻿namespace CSE3PAX.HelpClasses
+{
+    public class SubjectInstance
+    {
+        public string InstanceName { get; set; }
+        public string SubjectName { get; set; }
+        public string StartDate { get; set; } 
+        public string EndDate { get; set; } 
+    }
+
+}
+
--- CSE3PAX/Pages/Lecturer/LecturerIndex.cshtml.cs
+++ CSE3PAX/Pages/Lecturer/LecturerIndex.cshtml.cs
@@ -1,29 +1,60 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using CSE3PAX; 
 using Microsoft.AspNetCore.Authorization;
+using CSE3PAX.HelpClasses;
 
 namespace CSE3PAX.Pages.Lecturer
 {
-    [Authorize(Policy = "isLecturer")]
 
     public class LecturerIndexModel : PageModel
     {
-        //get current month and year 
-        //Array SubjectInstance {instance name, subject name, start date, end date,}
-        //Array SubjectInstances[]
 
-        //SQL Query to pull all subject instances over the next 12 months where lecturer id = lecturer id
-        // put each row into the subject instances array
+        //Current Table row headers
+        public List<string> Next12Months { get; set; } = new List<string>();
 
-        //For each subjectinstance in subjectinstances 
+        //SubjectInstance class in HelpClasses -> SubjectInstance.cs
+        public List<SubjectInstance> SubjectInstances { get; set; } = new List<SubjectInstance>();
+
+
+        public void OnGet()
+        {
+            //get current month and year 
+            DateTime now = DateTime.Now;
+
+            //add each month to table row headers.
+            for (int i = 0; i < 12; i++)
+            {
+                DateTime nextMonth = now.AddMonths(i);
+                string monthYearName = nextMonth.ToString("MMMM-yyyy");
+                //Console.WriteLine(monthYearName);
+                Next12Months.Add(monthYearName);
+            }
+            //Will need to connect to DB here.
+
+            //Object SubjectInstance will need to have {instance name, subject name, start date, end date,}
+           
+            // adding a SubjectInstance
+            SubjectInstances.Add(new SubjectInstance
+            {
+                InstanceName = "2023-MAT2DMX",
+                SubjectName = "DISCRETE MATHEMATICS FOR COMP SCI",
+                StartDate = "June-2024",
+                EndDate = "September-2024"
+            });
+
+
+            //SQL Query to pull all subject instances over the next 12 months where lecturer id = lecturer id
+            // put each row into the subject instances list
+
+            //For each subjectinstance in subjectinstances 
             //reorder by start date, if this cannot be done effectively by sql query
 
-        //get subjectinstance[0] start month
-        //create 12 column table based on start month adding +1 to month for each column. 
-        
-        //for each row subjectinstance in subject instances 
-            // 
 
-        
+        }
+
+        public void OnPost() { }
+       
+
+    
     }
 }
