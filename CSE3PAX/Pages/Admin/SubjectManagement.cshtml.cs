6b50899c562fca12567681edddb41c480ba67896
Charjah <charjah78@gmail.com>
2024-02-17 11:33:55 +1100

    Admin pages added
    Increased Admin doughnut size
    Added other admin interface pages

---
+++ CSE3PAX/Pages/Admin/SubjectManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSE3PAX.Pages.Admin
+{
+    public class SubjectManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
