a61373d6733133dd62376b9f0731c4528d75070d
michaellittle92 <michaellittle92@gmail.com>
2024-02-20 22:28:17 +1100

    Fixed bug allowing for multiple checkboxes to be selected.

--- CSE3PAX/wwwroot/js/CreateUser.js
+++ CSE3PAX/wwwroot/js/CreateUser.js
@@ -1,16 +1,37 @@
 ﻿const lecturerCheckBox = document.getElementById("IsLecturer");
+const managerCheckBox = document.getElementById("IsManager");
+const adminCheckBox = document.getElementById("IsAdmin");
 const lecturerFieldsDiv = document.getElementById("lecturerFieldsDiv");
 const lecturerFields = document.getElementsByClassName("requiredWhenVisible");
 
-// Initially hide the fields
 lecturerFieldsDiv.style.display = "none";
+
 
 lecturerCheckBox.addEventListener('change', function () {
     if (this.checked) {
+        // If lecturerCheckBox is checked, uncheck managerCheckBox and adminCheckBox set lecturer feilds to display
+        managerCheckBox.checked = false;
+        adminCheckBox.checked = false;
         lecturerFieldsDiv.style.display = "block";
-        Array.from(lecturerFields).forEach(field => field.required = true);
-    } else {
+    }
+});
+
+managerCheckBox.addEventListener('change', function () {
+    if (this.checked) {
+        // If managerCheckBox is checked, uncheck lecturerCheckBox
+        lecturerCheckBox.checked = false;
+        adminCheckBox.checked = false;
         lecturerFieldsDiv.style.display = "none";
-        Array.from(lecturerFields).forEach(field => field.required = false);
+
+
+    }
+});
+adminCheckBox.addEventListener('change', function () {
+    if (this.checked) {
+        // If managerCheckBox is checked, uncheck lecturerCheckBox
+        managerCheckBox.checked = false;
+        lecturerCheckBox.checked = false;
+        lecturerFieldsDiv.style.display = "none";
+
     }
 });
