3550aa818981cea029153b1a38e5d6bf2401b286
michaellittle92 <michaellittle92@gmail.com>
2024-02-21 18:34:49 +1100

    Fixed js link on create user.

--- CSE3PAX/obj/Debug/net7.0/CSE3PAX.AssemblyInfo.cs
+++ CSE3PAX/obj/Debug/net7.0/CSE3PAX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1a0fa36d000f5f031bf7848add8694ec014812")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
