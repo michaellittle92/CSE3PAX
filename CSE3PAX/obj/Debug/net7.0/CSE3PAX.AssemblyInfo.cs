5ec358ee936490f11e92b47961c4b18e1ab3209c
Charjah <charjah78@gmail.com>
2024-02-29 10:08:09 +1100

    Admin UI Styling
    Staff Management styling
    Generate report styling
    Subject Management styling

--- CSE3PAX/obj/Debug/net7.0/CSE3PAX.AssemblyInfo.cs
+++ CSE3PAX/obj/Debug/net7.0/CSE3PAX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdedf876c0ff6003ba349ab5b140209b383cef4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
