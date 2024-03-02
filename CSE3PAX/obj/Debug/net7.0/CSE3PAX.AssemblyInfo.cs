b88f4e95ecca284a2a7451f60abed52d5a701a6d
lsearle94 <laura-jean94@hotmail.com>
2024-03-02 13:44:30 +1100

    design changes

--- CSE3PAX/obj/Debug/net7.0/CSE3PAX.AssemblyInfo.cs
+++ CSE3PAX/obj/Debug/net7.0/CSE3PAX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a3950a1a4852727cb7644bd2f6452b5f104225")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE3PAX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
