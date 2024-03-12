577765cd00ff75ffc1acfabe64147abb5a1aa30b
Charjah <charjah78@gmail.com>
2024-03-12 12:23:57 +1100

    Admin Ui redirection links
    After edit redirection links

--- CSE3PAX/Pages/Shared/Settings.cshtml.cs
+++ CSE3PAX/Pages/Shared/Settings.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+
 
 namespace CSE3PAX.Pages.Shared
 {
     public class SettingsModel : PageModel
     {
+
+  
         public void OnGet()
         {
         }
