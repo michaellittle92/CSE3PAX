41f05c14468a88945a0482c7ebedb8e348536d69
Alexx Verdichizzi <52146857+alexxverd@users.noreply.github.com>
2024-04-06 13:51:44 +1100

    Update SubjectInstance.cs

--- CSE3PAX/HelpClasses/SubjectInstance.cs
+++ CSE3PAX/HelpClasses/SubjectInstance.cs
@@ -6,6 +6,8 @@
         public string SubjectName { get; set; }
         public string StartDate { get; set; } 
         public string EndDate { get; set; } 
+        public string? FullStartDate { get; internal set; }
+        public string? FullEndDate { get; internal set; }
     }
 
 }
